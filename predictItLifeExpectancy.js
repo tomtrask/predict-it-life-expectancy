673db38d6cd24144f4e4cf4030673dfe3a853111
Tom Trask <>
2017-10-11 09:00:15 -0400

    Initial commit.

---
+++ predictItLifeExpectancy.js
@@ -0,0 +1,142 @@
+const predictIt = require('predict-it');
+const fs = require("fs")
+const moment = require("moment")
+
+const JSON_FILE_NAME_ = "./predictit_markets.json"
+const DECIMAL_TIME_FMT_ = "YYYYMMDDHHmmss"
+const FRESHNESS_DATE_NAME_ = "freshUntil"
+
+const done = (msg) => {
+  console.log(msg)
+  process.exit()
+}
+
+const fetchMarketList = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(JSON_FILE_NAME_, "utf8", (err, data) => {
+      if (err) {
+        if (err.code == "ENOENT") {
+          // This is where the initial, empty file is defined
+          resolve({
+            freshUntil: parseInt(moment(0).format(DECIMAL_TIME_FMT_)),
+            symbolsOfInterest: [],
+            marketsOfInterest: {}
+          })
+        } else {
+          reject(err)
+        }
+      } else {
+        resolve(JSON.parse(data))
+      }
+    })
+  })
+}
+
+const writeMarketList = (data) => {
+  fs.writeFile(JSON_FILE_NAME_, JSON.stringify(data, {}, 4))
+}
+
+const mergeNewMarkets = (fileSettings, newMarketData) => {
+  let marketAbstracts = {}
+  let marketAbstractsCount = 0
+  let careAbout = {}
+  let dontCareAbout = {}
+  if (fileSettings.symbolsOfInterest) {
+    fileSettings.symbolsOfInterest.forEach(contractName => {
+      if (fileSettings.marketAbstracts[contractName]) {
+        careAbout[contractName] = fileSettings.marketAbstracts[contractName]
+      }
+    })
+  }
+  let marketsOfInterest = {}
+  newMarketData.forEach(market => {
+    if (market.Contracts.length == 1 && market.Contracts[0].Status == "Open") {
+      const contract = market.Contracts[0]
+      marketAbstracts[contract.TickerSymbol] = {
+        "id": contract.ID,
+        "shortName": contract.ShortName,
+        "ticker": contract.TickerSymbol,
+        "url": contract.URL
+      }
+      if (careAbout[contract.TickerSymbol]) {
+        marketsOfInterest[contract.TickerSymbol] = market
+      }
+      ++marketAbstractsCount
+    }
+  })
+  // TODO: we should have a way of forcing service call if new SoI
+  fileSettings.symbolsOfInterest = fileSettings.symbolsOfInterest.sort()
+  fileSettings.marketsOfInterest = marketsOfInterest
+  fileSettings.marketAbstracts = marketAbstracts
+}
+
+const PRETTY_DATE_FMT_ = "YYYY-MM-DD"
+
+describeInterestingContracts = (symbols, marketMap) => {
+  const now = moment()
+  const withinThreeMonths = moment().add(3,"months")
+  const withinSixMonths = moment().add(6,"months")
+  const withinTwelveMonths = moment().add(12,"months")
+
+  symbols.forEach(symbol => {
+    const market = marketMap[symbol]
+    const contract = market.Contracts[0]
+    const contractEndDate = moment(contract.DateEnd)
+    const daysRemaining = contractEndDate.diff(now, "days")+1
+    const survivalProb = contract.LastTradePrice
+    const dailySurvivalProb = Math.exp(Math.log(survivalProb)/daysRemaining)
+    const weeklySurvivalProb = Math.pow(dailySurvivalProb, 7)
+    const lifeExpectation = Math.log(0.5)/Math.log(dailySurvivalProb)
+    const deathDate = moment().add(lifeExpectation, "day")
+
+    console.log("Contract: "+symbol)
+    console.log("  Contract end: "+contractEndDate.format(PRETTY_DATE_FMT_))
+    console.log("  Days remaining: "+daysRemaining)
+    console.log("  Last trade: $"+contract.LastTradePrice)
+    console.log("  Weekly P(S): "+(100*weeklySurvivalProb).toFixed(1)+" %")
+    console.log("  Life expectancy: "+deathDate.format(PRETTY_DATE_FMT_))
+    if (deathDate < withinThreeMonths) {
+      console.log("  WITHIN 3 MONTHS")
+    } else if (deathDate < withinSixMonths) {
+      console.log("  WITHIN 6 MONTHS")
+    } else if (deathDate < withinTwelveMonths) {
+      console.log("  WITHIN 12 MONTHS")
+    }
+    console.log("")
+  })
+}
+
+const isSavedDatabaseIncomplete = (fileSettings) => {
+  // some will return true if any element caused the loop to return true
+  return fileSettings.symbolsOfInterest.some(symbol => {
+    return !(symbol in fileSettings.marketsOfInterest)
+  })
+}
+
+fetchMarketList().then(data => {
+  const nowDecimal = parseInt(moment().format(DECIMAL_TIME_FMT_))
+
+  if (data.freshUntil < nowDecimal || isSavedDatabaseIncomplete(data)) {
+    let fileSettings = data
+    console.log("HITTING SERVICE")
+    return predictIt.all().then(data => {
+      fileSettings.freshUntil = parseInt(moment().add(10,"minute")
+          .format(DECIMAL_TIME_FMT_))
+      mergeNewMarkets(fileSettings, data)
+      return fileSettings
+    })
+  } else {
+    console.log("Did NOT hit service")
+    return data
+  }
+}).then((newFileSettings) => {
+  describeInterestingContracts(newFileSettings.symbolsOfInterest,
+      newFileSettings.marketsOfInterest)
+  return newFileSettings
+}).then((finalSettings) => {
+  return writeMarketList(finalSettings)
+}).then(() => {
+  console.log("Post write clean up")
+}).catch(err => {
+  console.error("Error: "+err)
+})
